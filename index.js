f2197b78ed788087791b959493ff4248218956e0
Rakesh Rock <25bcad59@kristujayanti.com>
2026-04-02 11:59:12 +0530

    main1

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // DB CONNECTION
-const express = require("express");
-const mysql = require("mysql2");
+
 const cors = require("cors");
 
-const app = express();
+
 app.use(cors());
-app.use(express.json());
+
 
 // ✅ Railway DB connection
 const db = mysql.createConnection({
