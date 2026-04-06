d467d3d87cde240b7653bba29b58246ca63e5824
Rakesh Rock <25bcad59@kristujayanti.com>
2026-04-06 13:15:48 +0530

    msg

--- public/script.js
+++ public/script.js
@@ -24,8 +24,7 @@ function sendMessage() {
 }
 
 function loadMessages() {
-  fetch("https://priyanka-production.up.railway.app
-    /messages")
+  fetch("https://priyanka-production.up.railway.app/messages")
   .then(res => res.json())
   .then(data => {
     const box = document.getElementById("msgList");
